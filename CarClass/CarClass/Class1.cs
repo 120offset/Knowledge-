88b1c18187644b521055ba74a8844791fabdfef6
Miguel Sanchez <42881956+120offset@users.noreply.github.com>
2018-08-31 22:20:53 -0400

    Add files via upload

---
+++ CarClass/CarClass/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    public class Car
+    {
+      
+        public int YearModel { get; set; }
+        public string Make { get; set; }
+        public int Speed { get; set; }
+        //property
+
+
+        public Car(int yearModel, string make)
+        {
+            this.YearModel = yearModel;
+            this.Make = make;
+            this.Speed = 0;
+        }
+        //constructor
+
+        public Car()
+        {
+            this.Speed = 0;
+        }
+
+        public void Accelerate()
+        {
+            this.Speed += 5;
+        }
+        public void Brake()
+        {
+            this.Speed -= 5;
+        }
+        //method
+    }
+}
---
+++ CarClass/CarConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarClass;
+
+namespace CarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(1976, "Chevy Cheyene");
+            Console.WriteLine(car.Speed);
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                car.Accelerate();
+                Console.WriteLine(car.Speed);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                car.Brake();
+                Console.WriteLine(car.Speed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
