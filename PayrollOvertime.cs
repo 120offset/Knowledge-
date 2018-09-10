333ee3e830ae90634bdcfa9caa378bfcbb2cdc68
Miguel Sanchez <42881956+120offset@users.noreply.github.com>
2018-09-09 20:47:50 -0400

    Add files via upload

---
+++ PayrollOvertime.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CoffeeClass 
+{
+    public struct PayrollOvertime
+    {
+        //named constants 
+        const double baseHours = 40;
+        const double overtime = 1.5;
+
+        //local variables 
+        double hoursWorked;
+        double hourlyPayRate;
+        double overtimeHours;
+        double basePay;
+        double overtimePay1;
+        double overtimePay2;
+        double overtimePay3;
+        double overtimePay4;
+        double overtimePay5;
+        double grossPay1;
+        double grossPay2;
+        double grossPay3;
+        double grossPay4;
+        double grossPay5;
+      
+
+        public double EmployeePay1()
+        {
+            Console.WriteLine("\nHow many hours did the Employee 1 work this week?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                //get the hours worked and hourly pay rate 
+                hoursWorked = hours;
+                hourlyPayRate = 12.5;
+
+                //determine the gross pay 
+                if (hoursWorked > baseHours)
+                {
+                    //calculate the base pay (without overtime) 
+                    basePay = hourlyPayRate * baseHours;
+
+                    //calculate the number of overtime hours 
+                    overtimeHours = hoursWorked - baseHours;
+
+                    //calculate the overtime pay 
+                    overtimePay1 = overtimeHours * hourlyPayRate * overtime;
+
+                    //calculate the gross pay 
+                    grossPay1 = basePay + overtimePay1;
+                }
+                else
+                {
+                    //calculate the gross pay
+                    grossPay1 = hoursWorked * hourlyPayRate;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return this.grossPay1 + this.overtimePay1;
+        }
+        public double EmployeePay2()
+        {
+            Console.WriteLine("How many hours did the Employee 2 work this week?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                hoursWorked = hours;
+                hourlyPayRate = 12.5;
+                if (hoursWorked > baseHours)
+                {
+                    basePay = hourlyPayRate * baseHours;
+                    overtimeHours = hoursWorked - baseHours;
+                    overtimePay2 = overtimeHours * hourlyPayRate * overtime;
+                    grossPay2 = basePay + overtimePay2;
+                }
+                else
+                {
+                    grossPay2 = hoursWorked * hourlyPayRate;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return this.grossPay2 + this.overtimePay2;
+        }
+        public double EmployeePay3()
+        {
+            Console.WriteLine("How many hours did the Employee 3 work this week?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                hoursWorked = hours;
+                hourlyPayRate = 12.5;
+                if (hoursWorked > baseHours)
+                {
+                    basePay = hourlyPayRate * baseHours;
+                    overtimeHours = hoursWorked - baseHours;
+                    overtimePay3 = overtimeHours * hourlyPayRate * overtime;
+                    grossPay3 = basePay + overtimePay3;
+                }
+                else
+                {
+                    grossPay3 = hoursWorked * hourlyPayRate;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return this.grossPay3 + this.overtimePay3;
+        }
+        public double EmployeePay4()
+        {
+            Console.WriteLine("How many hours did the Employee 4 work this week?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                hoursWorked = hours;
+                hourlyPayRate = 12.5;
+                if (hoursWorked > baseHours)
+                {
+                    basePay = hourlyPayRate * baseHours;
+                    overtimeHours = hoursWorked - baseHours;
+                    overtimePay4 = overtimeHours * hourlyPayRate * overtime;
+                    grossPay4 = basePay + overtimePay4;
+                }
+                else
+                {
+                    grossPay4 = hoursWorked * hourlyPayRate;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return this.grossPay4 + this.overtimePay4;
+        }
+        public double EmployeePay5()
+        {
+            Console.WriteLine("How many hours did the Employee 5 work this week?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                hoursWorked = hours;
+                hourlyPayRate = 12.5;
+                if (hoursWorked > baseHours)
+                {
+                    basePay = hourlyPayRate * baseHours;
+                    overtimeHours = hoursWorked - baseHours;
+                    overtimePay5 = overtimeHours * hourlyPayRate * overtime;
+                    grossPay5 = basePay + overtimePay5;
+                }
+                else
+                {
+                    grossPay5 = hoursWorked * hourlyPayRate;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return this.grossPay5 + this.overtimePay5;
+        }
+
+
+        public double PayRoll()
+        {
+            //calculate payroll
+            double PayRoll = grossPay1 + grossPay2 + grossPay3 + grossPay4 + grossPay5;
+            Console.WriteLine($"The week's Payroll is {PayRoll:c}");
+            return PayRoll;
+        }
+        public double TotalOvertime()
+        {
+            //calculate overtime
+            double TotalOvertime = overtimePay1 + overtimePay2 + overtimePay3 + overtimePay4 + overtimePay5;
+            Console.WriteLine($"The total Overtime paid this week is {TotalOvertime:c}");
+            Console.ReadLine();
+            return TotalOvertime;
+        }
+    }
+}
---
+++ Struct.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeClass
+{
+    public class Coffee
+    {
+        double CoffeeSales { get; set; }
+
+        //menu items
+        const double csmprice = 1;
+        const double cmdprice = 3;
+        const double clgprice = 5;
+        const double csmcost = .10;
+        const double cmdcost = .15;
+        const double clgcost = .20;
+        const double eggprice = 5.50;
+        const double chxprice = 7.50;
+        const double eggcost = .95;
+        const double chxcost = 1.20;
+        
+        public void GetSales()
+        {
+            
+
+            try
+            {
+                Console.WriteLine("How many Small Coffees were sold?");
+                double small = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("How many Medium Coffees were sold?");
+                double medium = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("How many Large Coffees were sold?");
+                double large = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("How many Egg Sandwiches were sold?");
+                double egg = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("How many Chicken Biscuits were sold?");
+                double chx = Convert.ToDouble(Console.ReadLine());
+                
+                //constant menu price w daily items
+                double TotalCost = (small * csmcost) + (medium * cmdcost) + (large * clgcost) + (egg * eggcost) + (chx * chxcost);
+                double TotalSales = (small * csmprice) + (medium * cmdprice) + (large * clgprice) + (egg * eggcost) + (chx * chxprice);
+                double TotalProfit = TotalSales - TotalCost;
+
+                Console.WriteLine($"Total Sales for the day: {TotalSales:c}\nTotal Cost for the day: {TotalCost:c}\nTotal Profit for the day: {TotalProfit:c}");
+            }
+            catch (Exception ex)
+            {
+                if (ex is Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+        }
+    }
+    public enum Option { Sales = 1, PayRoll, Exit }
+}
+
