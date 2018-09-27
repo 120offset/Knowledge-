05be4822dcceefddc2befc534455b3fbfcd8e70c
Miguel Sanchez <42881956+120offset@users.noreply.github.com>
2018-09-26 23:27:43 -0400

    Add files via upload

--- ErrorsandExceptions/ErrorsandExceptions/Program.cs
+++ ErrorsandExceptions/ErrorsandExceptions/Program.cs
@@ -4,162 +4,265 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ErrorsandExceptions
+
+
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("[a] enter this option if you would like to work with circles.");
-            Console.WriteLine("[b] enter this option if you woule like to work with triangles.");
-            Console.WriteLine("[c] enter this option if you woule like to work with hemispheres.");
-            Console.WriteLine("[d] enter this option if you woule like to work with the quadratic formula.");
-            Console.WriteLine("[q] enter this option if you would like to end the project.");
-            try
+           
+            //shortern version to initialize
+            int[] ArrayA = { 0, 2, 4, 6, 8, 10 };
+            int[] ArrayB = { 1, 3, 5, 7, 9 };
+            int[] ArrayC = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+            //initialize array length
+            int a = ArrayA.Length;
+            int b = ArrayB.Length;
+            int c = ArrayC.Length;
+
+            //Introduction
+            Console.WriteLine("\nArray A: 0,2,4,6,8,10");
+            Console.WriteLine("Array B: 1,3,5,7,9");
+            Console.WriteLine("Array C: 3,1,4,1,5,9,2,6,5,3,5,9\n\n");
+            
+
+            //the count for each array
+            Console.WriteLine($"Here are the counts of each array.\n\nThe count of Array A is {ArrayA.Length}");
+            Console.WriteLine($"The count of Array B is {ArrayB.Length}");
+            Console.WriteLine($"The count of Array C is {ArrayC.Length}");
+
+            //print the sum of arrays
+            Console.WriteLine("\nHere are the sums of each array.\n\nSum of Array A is " + sumA(ArrayA, a));
+            Console.WriteLine("Sum of Array B is " + sumB(ArrayB, b));
+            Console.WriteLine("Sum of Array C is " + sumC(ArrayC, c));
+
+            //print the mean of arrays
+            Console.WriteLine("\nHere are the means of each array.\n\nMean of Array A is " + MeanA(ArrayA, a));
+            Console.WriteLine("Mean of Array B is " + MeanB(ArrayB, b));
+            Console.WriteLine("Mean of Array C is " + MeanC(ArrayC, c));
+
+            //reverse Arrays 
+            rverseArrayA(ArrayA, 0, 5);
+            Console.WriteLine("\nEach arrays reversed.\n\nArray A\n");
+            printrvers(ArrayA, 6);
+
+            rverseArrayB(ArrayB, 0, 4);
+            Console.WriteLine("Array B\n");
+            printrvers(ArrayB, 5);
+
+            rverseArrayC(ArrayC, 0, 11);
+            Console.WriteLine("Array C\n");
+            printrversC(ArrayC, 12);
+
+            //rotate arrays
+            Console.WriteLine("\nRotated Arrays");
+            Console.WriteLine("Array C rotated two places to the Left\n");
+            PrintArray(ArrayRotator(2, true, ArrayA));
+            Console.WriteLine("\nArray B rotated two places to the Right\n");
+            PrintArray(ArrayRotator(2, false, ArrayB));
+            Console.WriteLine("\nArray C rotated four places to the Left\n");
+            PrintArray(ArrayRotator(4, true, ArrayC));
+            Console.WriteLine();
+
+            //sorted arrays
+            Console.WriteLine("\nArray C is sorted\n");
+            PrintArray(SortArr(ArrayC));
+            
+        }
+        //method for sum of arrays
+        static int sumA(int[] ArrayA, int a)
+        {
+            int sum = 0;
+            //iterate through all elements and add to sum
+            for (int i = 0; i < a; i++)
+                sum += ArrayA[i];
+            return sum;
+        }
+        static int sumB(int[] ArrayB, int b)
+        {
+            int sum = 0;
+            for (int i = 0; i < b; i++)
+                sum += ArrayB[i];
+            return sum;
+        }
+        static int sumC(int[] ArrayC, int c)
+        {
+            int sum = 0;
+            for (int i = 0; i < c; i++)
+                sum += ArrayC[i];
+            return sum;
+        }
+
+        //MEAN OF ARRAYS 
+
+        static double MeanA(int[] ArrayA, int a)
+        {
+            double sum = 0, mean = 0;
+            for (int i = 0; i < a; i++)
             {
-                string option = Console.ReadLine();
+                sum += ArrayA[i];
+            }
+            mean = sum / a;
+            return mean;
+        }
+        static double MeanB(int[] ArrayB, int b)
+        {
+            double sum = 0, mean = 0;
+            for (int i = 0; i < b; i++)
+            {
+                sum += ArrayB[i];
+            }
+            mean = sum / b;
+            return mean;
+        }
+        static double MeanC(int[] ArrayC, int c)
+        {
+            double sum = 0, mean = 0;
+            for (int i = 0; i < c; i++)
+            {
+                sum += ArrayC[i];
+            }
+            mean = sum / c;
+            return mean;
+        }
 
-                switch (option)
+        //REVERSE ARRAY
+        static void rverseArrayA(int[] ArrayA, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = ArrayA[start];
+                ArrayA[start] = ArrayA[end];
+                ArrayA[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        static void printrvers(int[] ArrayA, int size)
+        {
+            string values = "";
+
+            for (int i = 0; i < size; i++)
+            {
+                values +=  ArrayA[i] + " ";
+            }
+                Console.WriteLine(values);
+            Console.WriteLine();
+        }
+        static void rverseArrayB(int[] ArrayB, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = ArrayB[start];
+                ArrayB[start] = ArrayB[end];
+                ArrayB[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        static void printrversB(int[] ArrayB, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.WriteLine(ArrayB[i] + "");
+            Console.WriteLine();
+        }
+        static void rverseArrayC(int[] ArrayC, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = ArrayC[start];
+                ArrayC[start] = ArrayC[end];
+                ArrayC[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        static void printrversC(int[] ArrayC, int size)
+        {
+            string values = "";
+            for (int i = 0; i < size; i++)
+            {
+                values += ArrayC[i] + " ";
+            }
+                Console.WriteLine(values);
+            Console.WriteLine();
+        }
+
+        //PRINTED ARRAY METHOD FOR ROTATE AND SORT
+        static void PrintArray(int[] array)
+        {
+            
+            foreach (int item in array)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+
+        //ROTATING ARRAY
+        static int [] ArrayRotator(int cycles, bool direction, int[] choiceArray)
+        {
+            int[] ArrayRotator = new int[choiceArray.Length];
+
+            //True rotate right
+            //False rotate left
+
+            if (direction == true)
+            {
+                int ii = 0;
+                for (int i = 0; i < choiceArray.Length; i++)
                 {
+                    if (choiceArray.Length - ii - cycles == choiceArray.Length)
+                    {
+                        ii = choiceArray.Length - cycles;
+                    }
+                    ArrayRotator[i] = choiceArray[choiceArray.Length - ii - cycles];
 
-                    case "a":
-                        Console.WriteLine("\nLet's find both the circumference and area of a circle");
-                        Console.WriteLine("\nOk,\nWhat is the radius of your circle?\n");
-                        double r = Convert.ToDouble(Console.ReadLine());
-
-                        CircleAreaCircumference(r);
-                        Console.ReadLine();
-
-                        break;
-                    case "b":
-                        Console.WriteLine("\nLet's find the area of a triangle");
-                        Console.WriteLine("You're going to have to provide the length of 3 sides");
-
-                        Console.Write("\nPlease enter side 1 then enter\n");
-                        int side1;
-                        side1 = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("\nPlease enter side 2 then enter\n");
-                        int side2;
-                        side2 = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("\nPlease enter side 3 then enter\n");
-                        int side3;
-                        side3 = Convert.ToInt32(Console.ReadLine());
-
-                        AreaTriangle(side1, side2, side3);
-                        Console.ReadLine();
-
-                        break;
-                    case "c":
-                        Console.WriteLine("\nLet's calculate the volume of a hemisphere of any sphere you want");
-                        Console.WriteLine("\nEnter a radius, please\n");
-                        double r1;
-                        r1 = Convert.ToDouble(Console.ReadLine());
-
-                        HemisphereVolume(r1);
-                        Console.ReadLine();
-                        break;
-                    case "d":
-                        double a, b, c;
-
-                        Console.WriteLine("\nThis program will solve the quadratic formula for you");
-                        Console.WriteLine("First you will need to give three variables");
-
-                        Console.WriteLine("\nPlease enter the value for a:");
-                        a = double.Parse(Console.ReadLine());
-
-                        Console.WriteLine("\nPlease enter the value for b:");
-                        b = double.Parse(Console.ReadLine());
-
-                        Console.WriteLine("\nPlease enter the value for c:");
-                        c = double.Parse(Console.ReadLine());
-
-                        QuadFormula(a, b, c);
-                        Console.ReadLine();
-                        break;
-                    case "q":
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Have a nice day :)");
-                        Console.ReadLine();
-                        Environment.Exit(0);
-                        break;
+                    ii--;
                 }
             }
-            catch (FormatException fEx)
+            else if (direction == false)
             {
-                Console.WriteLine(fEx.Message);
+                int ii = 0;
+                for (int i = 0; i < choiceArray.Length; i++)
+                {
+                    if (ii + cycles == choiceArray.Length)
+                    {
+                        ii = 0 - cycles;
+                    }
+                    ArrayRotator[i] = choiceArray[ii + cycles];
+
+                    ii++;
+                }
             }
-            catch (DivideByZeroException DBZE)
-            {
-                Console.WriteLine(DBZE.Message);
-            }
-            catch (OverflowException OvFlow)
-            {
-                Console.WriteLine(OvFlow.Message);
-            }
-            catch (Exception Excp)
-            {
-                Console.WriteLine(Excp.Message);
-            }
-            finally
-            {
-                Console.WriteLine("This program has finally terminated.");
-            }
-        }
-        static void CircleAreaCircumference(double r)
+            return ArrayRotator;
+        } 
+
+        //SORTING ARRAYS
+        static int[] SortArr(int[] sortArray)
         {
-            double c = 2 * Math.PI * r;
-            c = Math.Round(c, 2);
-            Console.WriteLine($"\nThe circumference is {c}");
+            int num = 0;
 
-            // This will display the area
-            double a = Math.PI * (r * r);
-            a = Math.Round(a, 2);
-            Console.WriteLine($"\nThe area is {a}\n");
-
-
+            for (int i = 0; i < sortArray.Length - 1; i++)
+            {
+                for (int sort = 0; sort < sortArray.Length - 1 - i; sort++)
+                {
+                    if (sortArray[sort] > sortArray[sort + 1])
+                    {
+                        num = sortArray[sort];
+                        sortArray[sort] = sortArray[sort + 1];
+                        sortArray[sort + 1] = num;
+                    }
+                }
+            
+            }
+            return sortArray;
         }
-        static void AreaTriangle(int side1, int side2, int side3)
-        {
-            double s;
-            s = (side1 + side2 + side3) / 2;
-            double a;
-            a = Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
-            Console.WriteLine($"\nThe area of the Triangle is {a}");
-            Console.ReadLine();
-        }
-        static void HemisphereVolume(double r1)
-        {
-            double v = (2.0 / 3) * Math.PI * r1 * r1 * r1;
-            v = Math.Round(v, 2);
-            Console.WriteLine($"\nThe volume of a hemisphere is {v}");
-        }
-        static void QuadFormula(double a, double b, double c)
-        {
-            double inside = (b * b) - 4 * a * c;
-
-            // The equation involves a +- so there will be two solutions 
-            double root1;
-            root1 = (-b + (Math.Sqrt(inside) / (2 * a)));
-
-            double root2;
-            root2 = (-b - (Math.Sqrt(inside) / (2 * a)));
-
-            // This part will convert the two root results to strings
-            string r1 = Convert.ToString(root1);
-            string r2 = Convert.ToString(root2);
-
-            // This part will show the answer
-
-            Console.WriteLine($"Root 1 ={r1} and Root 2 = {r2}");
-        }
-        //static void LessthanZero(double one, string two)
-        //{
-    
-        //}
-
-        
 
     }
 }
